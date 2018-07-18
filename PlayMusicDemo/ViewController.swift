8dd7367349e35f3c8aa30587d0671e76374ccac2
mingsc <KevinChen.tw@gmail.com>
2018-07-18 22:26:28 +0800

    Initial Commit

---
+++ PlayMusicDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayMusicDemo
+//
+//  Created by Ming hsien Chen on 2018/7/18.
+//  Copyright © 2018年 Ming hsien Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
